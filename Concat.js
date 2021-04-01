67daeb6eff2c275152804560b15f4180646409e7
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-04-01 23:34:14 +0530

    JS Fundamentals

--- Concat.js
+++ Concat.js
@@ -4,11 +4,11 @@ let array2 = ['March','April','Jan','Feb','May'];
 
 const array3 = array1.concat(array2);
 
-// console.log(array3);        //      to concat to arrays
+console.log(array3);        //      to concat to arrays
 
-// console.log([...array1, ...array2]);        //    to concat to arrays
+console.log([...array1, ...array2]);        //    to concat to arrays
 
 
-// Join Method
+Join Method
 
 console.log(array3.join('-'));
